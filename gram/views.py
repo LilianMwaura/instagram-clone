17f79f986b8760068b98e2eb3fcdba2e10ec415e
LilianMwaura <wambuimwaura28@gmail.com>
2022-06-05 09:35:48 +0300

    add views imports

--- gram/views.py
+++ gram/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.http  import HttpResponse,Http404,HttpResponseRedirect
+from .models import Image, Profile, Like
+from django.contrib.auth.models import User
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import Http404
+from .forms import ImageForm, ProfileForm, CommentForm
+from django.contrib.auth import logout# Create your views here.
+from .email import 
 
 # Create your views here.
