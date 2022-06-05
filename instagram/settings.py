67b15289e29572ba4f97d9e256355487b3441ad1
LilianMwaura <wambuimwaura28@gmail.com>
2022-06-05 11:57:22 +0300

    update installed apps

--- instagram/settings.py
+++ instagram/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'gram.apps.GramConfig',
+    'cloudinary'
 ]
 
 MIDDLEWARE = [
