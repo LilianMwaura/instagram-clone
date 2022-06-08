9f87f5b4f2c61cdde3b9f2ed6ae6bd25e613014c
LilianMwaura <wambuimwaura28@gmail.com>
2022-06-08 07:08:39 +0300

    update settings config

--- instagram/settings.py
+++ instagram/settings.py
@@ -10,7 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -85,7 +89,11 @@ DATABASES = {
         'PASSWORD': '1234'
     }
 }
-
+cloudinary.config( 
+  cloud_name = "wambui", 
+  api_key = "824351776517753", 
+  api_secret = "bNqxpHAJwFY-im5zSlnm8FVodXY" 
+)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -122,6 +130,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
