a558267fff96d9c3aaebb8b5d0c515447c996c06
LilianMwaura <wambuimwaura28@gmail.com>
2022-06-08 07:32:46 +0300

    register admin models

--- gram/admin.py
+++ gram/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+class ImageAdmin(admin.ModelAdmin):
+    filter_horizontal =('categories',)
+    
+admin.site.register(Profile)
+admin.site.register(Like)
+admin.site.register(Comment)
+admin.site.register(Image)
